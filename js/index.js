d16e947d13b90d9d5dc3b0d761ab367fbd06ab6b
17601330731 <17601330731@163.com>
2019-11-25 10:13:29 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+window.onscroll = function() {
+  //为了保证兼容性，这里取两个值，哪个有值取哪一个
+  //scrollTop就是触发滚轮事件时滚轮的高度
+  var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  console.log("滚动距离" + scrollTop);
+}
+
